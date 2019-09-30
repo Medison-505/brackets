bad4a4c9acb2cfb16a777d028f76f1615ca3fb56
Alena Skobeleva <askobeleva@affmy.com>
2019-09-30 17:33:46 +0300

    init: done brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let string = str.split('');
+    let data = [];
+    for(let b = 0; b < string.length; b++){
+        let char = string[b];
+        for (let i = 0; i < bracketsConfig.length; i++){
+            let open = bracketsConfig [i][0];
+            let close = bracketsConfig [i][1];
+            let last = data[data.length-1];
+            if (open == close){
+                if (char == open){
+                    if (last == open) {
+                        data.splice(-1,1);
+                    } else if (last != open){
+                        data.push(char);
+                    } else {
+                        if (data.length == 0){
+                            return false;
+                        }
+                    }
+                }
+
+            } else {
+                if (open == char) {
+                    data.push(char);
+                }
+                if (close == char) {
+                    if (data.length == 0){
+                        return false;
+                    }
+                    if (last == open){
+                        data.splice(-1,1);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    if (data.length == 0){
+        return true;
+    } else {
+        return false;
+    }
+
 }
